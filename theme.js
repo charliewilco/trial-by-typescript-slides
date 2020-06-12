aa5c47f2139b39a8e2ca12a9a9c7217287bdf73e
Charles ⚡ <charlespeters42@gmail.com>
2020-06-11 21:39:22 -0700

    adding other themes

--- theme.js
+++ theme.js
@@ -1,16 +1,15 @@
-import dark from "@mdx-deck/themes/dark";
-import prism from "react-syntax-highlighter/dist/styles/prism/duotone-space.js";
-import { pre, getLanguage } from "@mdx-deck/themes/syntax-highlighter";
+import dark from '@mdx-deck/themes/dark';
+import prism from 'react-syntax-highlighter/dist/styles/prism/duotone-space.js';
+import {pre, getLanguage} from '@mdx-deck/themes/syntax-highlighter';
 
-import SyntaxHighlighter from "react-syntax-highlighter";
+import SyntaxHighlighter from 'react-syntax-highlighter';
 
 export const code = props => {
   const language = getLanguage(props.className);
-  console.log(prism);
   const style = Object.assign({}, prism, {
     "pre[class*='language-']": {
-      fontSize: "inherit"
-    }
+      fontSize: 'inherit',
+    },
   });
   return <SyntaxHighlighter language={language} {...props} style={prism} />;
 };
@@ -18,12 +17,12 @@ export const code = props => {
 export const highlight = {
   pre,
   getLanguage,
-  code
+  code,
 };
 
 const theme = Object.assign({}, dark, {
-  font: "system-ui, sans-serif",
-  monospace: '"Operator Mono", monospace !important'
+  font: 'system-ui, sans-serif',
+  monospace: '"Operator Mono", monospace !important',
 });
 
 export default theme;
